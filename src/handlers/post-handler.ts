6cd168fdaa699a10ee1228276c1dbc9e191874bf
Tima Vysotski <vysotski.tima@gmail.com>
2019-09-27 20:49:39 +0300

    Entry point

--- src/handlers/post-handler.ts
+++ src/handlers/post-handler.ts
@@ -1,6 +1,16 @@
 import express from "express";
 import models from "../models/index";
-import checkcheckAuthentication, { checkAuthentication } from "../handlers/user-handler";
+import { checkAuthentication } from "../handlers/user-handler";
+import { MongoClient } from "mongodb";
+import { Mongoose } from "mongoose";
+
+interface UpdateRequest extends express.Request {
+  body: {
+    id?: string;
+    text?: string;
+    postedBy?: string
+  };
+};
 
 export default class Post {
 
@@ -10,53 +20,64 @@ export default class Post {
   }
 
   async showCurrentPost({ body: id }: express.Request, res: express.Response) {
-    const result = await models.post.findOne({ 'id': id });
+    const result = await models.post.findOne({ id });
+    res.send(result);
+  }
+  
+
+  async createPost({ body }: UpdateRequest, res: express.Response) {
+    const result = await models.post.create(body);
     res.send(result);
   }
 
-  async createPost(req: express.Request, res: express.Response) {
-    const result = await models.post.create({
-      text: req.body.text,
-      postedBy: req.body.postedBy
-    });
-    res.send(result);
-  }
+  async updatePost({ body: { id : _id, text, postedBy } }: UpdateRequest, res: express.Response) {
+    const result = await models.post.findOne({ _id });
 
-  async updatePost(req: express.Request, res: express.Response) {
-    const result = await models.post.findOne({ _id: req.body.id });
-    if (result) {
-      const currentUser = await models.user.findOne({ _id: result.postedBy });
-      if (currentUser) {
-        if (currentUser.isAuthenticated) {
-          models.post.findByIdAndUpdate(result.id, {
-            text: req.body.text
-          }, { new: true }, (err, result) => {
-            if (err) return console.log(err);
-          });
-          res.send("success!!");
-        } else {
-          res.send("You need to login before updating");
-        }
-      } else {
-        res.send("Issues with User");
-      }
-    } else {
-      res.send("Check id of the post");
-    }
-
-
-
-  }
-
-  async deletePost(req: express.Request, res: express.Response) {
-    const result = await models.post.findOne({ _id: req.body.id });
     if (result && checkAuthentication(result.postedBy)) {
-      models.post.findByIdAndDelete(result._id, (err, doc) => {
-        err ? console.log(err) : res.send("success");
+      models.post.findByIdAndUpdate(result.id, {
+        text: text
+      }, { new: true }, (err, result) => {
+        err ? console.log(err) : res.send("success!!");
       });
     } else {
-      res.send("Error");
+      res.send("Error")
     }
+  }
+
+  notEmpty(...params : Array<any>){
+    params.forEach(it => {
+      if (!it){
+        throw 'empty param';
+      }
+    })
+  }
+
+  async deletePost({ body: { id } }: UpdateRequest, res: express.Response) {
+    try {
+      this.notEmpty(id);
+
+      await models.post.findByIdAndDelete(id, (err) => {
+        if (err) {
+          throw err;
+        }
+
+        res.status(200).send();
+      });
+
+    } catch (error) {
+      res.status(400).send({ message: error });
+    }
+
+
+
+    // const result = await models.post.findOne({ _id });
+    // if (result && checkAuthentication(result.postedBy)) {
+    //   models.post.findByIdAndDelete(result._id, (err, doc) => {
+    //     err ? console.log(err) : res.send("success");
+    //   });
+    // } else {
+    //   res.send("Error");
+    // }
   }
 
   async deleteAllPostOfUser(req: express.Request, res: express.Response) {
