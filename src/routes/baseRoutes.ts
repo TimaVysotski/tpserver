44b0dd49d41cfdbd2c2bc268254bc2dcebbfe956
Tima Vysotski <vysotski.tima@gmail.com>
2019-10-10 12:05:17 +0300

    Test without Tokens

--- src/routes/baseRoutes.ts
+++ src/routes/baseRoutes.ts
@@ -10,15 +10,15 @@ const jwt = new JwtMiddelware();
 
 const configurateRoutes = {
     init: (app: express.Application) => {
-        app.use("/api", (req: express.Request, res: express.Response, next: express.NextFunction) => {
-            const token: String = jwt.checkHeaders(req);
-            jwt.checkToken({ token } as IToken)
-                .then(token => {
-                    console.log(token);
-                    next();
-                    })
-                .catch(error => res.status(STATUS_NOT_FOUND).send("U Must login!"));
-        });
+        // app.use("/api", (req: express.Request, res: express.Response, next: express.NextFunction) => {
+        //     const token: String = jwt.checkHeaders(req);
+        //     jwt.checkToken({ token } as IToken)
+        //         .then(token => {
+        //             console.log(token);
+        //             next();
+        //             })
+        //         .catch(error => res.status(STATUS_NOT_FOUND).send("U Must login!"));
+        // });
         app.use(API_ROUTES.USER_API, UserRoutes);
         app.use(API_ROUTES.SIGN_API, SignRoutes);
         app.use(API_ROUTES.POST_API, PostRoutes);
